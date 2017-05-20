398906a12a6a561b0608bfae649a934d79a5ea8e
Gabriel <wqq977sz@163.com>
2017-05-20 20:23:27 +0800

    Factorial: Make results more lovely

--- math/factorial/src/factorial.kt
+++ math/factorial/src/factorial.kt
@@ -2,7 +2,7 @@ import java.math.BigInteger
 
 class Result(var value: BigInteger = BigInteger.valueOf(1L))
 
-tailrec fun factorial(num: Int, result: Result){
+tailrec fun factorial(num: Int, result: Result) {
 	if (num == 0) result.value = result.value.times(BigInteger.valueOf(1L))
 	else {
 		result.value = result.value.times(BigInteger.valueOf(num.toLong()))
@@ -11,10 +11,22 @@ tailrec fun factorial(num: Int, result: Result){
 }
 
 fun main(args: Array<String>) {
+	val _PROGRAM_INFO = """
+********************************************
+*               Factorial                  *
+*                                          *
+*  Gabriel: wqq977sz@163.com               *
+*  https://github.com/Gabirel              *
+*                             2017/05/20   *
+*                                          *
+********************************************
+"""
+	println(_PROGRAM_INFO)
 	val result = Result()
 	if (args.size == 0) {
 		println("Please add an parameter!!")
-	} else{
+	} else {
+		println("${args[0].toInt()}! =")
 		factorial(args[0].toInt(), result)
 		println(result.value)
 	}
