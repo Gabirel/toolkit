6fdbf23748e868fa400f6304640c7bd4d5bf8028
Gabriel <wqq977sz@163.com>
2017-05-20 19:53:41 +0800

    Add factorial

---
+++ math/factorial/src/factorial.kt
@@ -0,0 +1,18 @@
+import java.math.BigInteger
+
+class Result(var value: BigInteger = BigInteger.valueOf(1L))
+
+tailrec fun factorial(num: Int, result: Result){
+	if (num == 0) result.value = result.value.times(BigInteger.valueOf(1L))
+	else {
+		result.value = result.value.times(BigInteger.valueOf(num.toLong()))
+		factorial(num - 1, result)
+	}
+}
+
+fun main(args: Array<String>) {
+	val result = Result()
+	//factorial(10000, result)
+	factorial(args[0].toInt(), result)
+	println(result.value)
+}
